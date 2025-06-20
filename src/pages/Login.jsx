1c7f180b0808d69d46639064f2424aca56260092
Tailhan Celine <tailhan.celine@hotmail.fr>
2025-06-20 12:59:10 +0200

    test coucou

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -56,7 +56,7 @@ export default function Login({ onLogin }) {
         </div>
       )}
 
-      <label className="block text-darkBlue font-semibold">Email</label>
+      <label className="block text-darkBlue font-semibold">Emailcoucou</label>
       <input
         value={email}
         onChange={(e) => setEmail(e.target.value)}
